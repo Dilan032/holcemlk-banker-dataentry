0148c64333bf8c4d3f43a9ad4cf62ae78a36f32c
Dilan kanishka <dilankanishka032@gmail.com>
2024-11-09 17:31:09 +0530

    update

--- controllers/showDetails/searchClientIDforNameController.js
+++ controllers/showDetails/searchClientIDforNameController.js
@@ -4,6 +4,11 @@ exports.searchClient = (req, res) => {
     const data = req.body;
     const inputCustomerID = data.CustomerID;
 
+    // Check if Customer ID is provided, otherwise send an error
+    if (!inputCustomerID) {
+        return res.status(400).json({ message: 'Please provide the Customer ID to search' });
+    }
+
     db.query( // show result regardin first letter
         'SELECT CustomerID, CustomerName FROM customerinformation WHERE CustomerID LIKE ?', [`${inputCustomerID}%`], 
         (error, result) => {
